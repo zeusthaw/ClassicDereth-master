2117dc208e273dd780d20ebdbea40a7fe021a2ab
Zeus <fleemdog2004@gmail.com>
2018-10-22 12:11:53 -0400

    BroadcastLocal Fix for Augmentations
    forgot to add it :O

--- Source/World.cpp
+++ Source/World.cpp
@@ -877,6 +877,14 @@ void CWorld::BroadcastGlobal(void *_data, DWORD _len, WORD _group, DWORD ignore_
 	}
 }
 
+void CWorld::BroadcastLocal(DWORD cellid, std::string text)
+{
+	BinaryWriter *textMsg = ServerText(text.c_str(), LTT_DEFAULT);
+	g_pWorld->BroadcastPVS(cellid, textMsg->GetData(), textMsg->GetSize(), PRIVATE_MSG, 0, false);
+	delete textMsg;
+}
+
+
 void CWorld::BroadcastLocal(DWORD cellid, std::string text, LogTextType channel)
 {
 	BinaryWriter *textMsg = ServerText(text.c_str(), channel);
