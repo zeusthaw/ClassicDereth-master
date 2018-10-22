a3898ac77e32a8cd014070cad351bb237d2a3a8e
Zeus <fleemdog2004@gmail.com>
2018-10-21 21:05:31 -0400

    Recipes no longer send fail/success msg if empty
    Merged from GDLE2 Test Server
    
    ```Recipes no longer send text to the client if the success or fail message is empty.
    ```
    
    https://gitlab.com/Scribble/gdlenhanced/commit/b3eb35379f8ddaa0ba92367d4a242413cbd0bf12

--- Source/Player.cpp
+++ Source/Player.cpp
@@ -1409,7 +1409,9 @@ int CPlayerWeenie::UseEx(bool bConfirmed)
 				return WERROR_NONE;
 			}
 
-			SendText(op->_successMessage.c_str(), LTT_CRAFT);
+			if (op->_successMessage != "")
+				SendText(op->_successMessage.c_str(), LTT_CRAFT);
+
 			
 			// Broadcast messages for tinkering
 			switch (op->_SkillCheckFormulaType)
@@ -1472,7 +1474,9 @@ int CPlayerWeenie::UseEx(bool bConfirmed)
 				return WERROR_NONE;
 			}
 
-			SendText(op->_failMessage.c_str(), LTT_CRAFT);
+			if (op->_failMessage != "")
+				SendText(op->_failMessage.c_str(), LTT_CRAFT);
+
 
 			// Broadcast messages for tinkering
 			switch (op->_SkillCheckFormulaType)
